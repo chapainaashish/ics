0125b22063f4bdd621f06dff5fcc6ab8918b6776
Aashish Chapain <ashishchapain86@gmail.com>
2024-07-21 14:28:59 +0545

    fixed changes

--- parser/parseicalendar.py
+++ parser/parseicalendar.py
@@ -1,6 +1,5 @@
-import calendar
 import json
-from datetime import datetime
+from datetime import date, datetime
 
 from icalendar import Calendar
 
@@ -8,13 +7,39 @@ from icalendar import Calendar
 def format_value(value):
     if isinstance(value, datetime):
         return value.strftime("%Y-%m-%dT%H:%M:%S")
+    elif isinstance(value, date):
+        return value.strftime("%Y-%m-%d")
     elif isinstance(value, (list, tuple)):
         return [format_value(v) for v in value]
     elif isinstance(value, dict):
         return {k: format_value(v) for k, v in value.items()}
     elif hasattr(value, "to_ical"):
-        return value.to_ical().decode()
+        return (
+            value.to_ical().decode()
+            if isinstance(value.to_ical(), bytes)
+            else value.to_ical()
+        )
     return value
+
+
+def get_attendees(attendees, role, partstat=None):
+    result = []
+    if isinstance(attendees, str):
+        if (role == "REQ-PARTICIPANT") and (partstat is None):
+            result.append(attendees.replace("mailto:", ""))
+    else:
+        if not isinstance(attendees, list):
+            attendees = [attendees]
+
+        for attendee in attendees:
+            # Default role to 'REQ-PARTICIPANT' if not specified
+            attendee_role = attendee.params.get("ROLE", "REQ-PARTICIPANT")
+            if attendee_role == role:
+                status = attendee.params.get("PARTSTAT", "NEEDS-ACTION")
+                if partstat is None or status == partstat:
+                    result.append(attendee.to_ical().decode().replace("mailto:", ""))
+
+    return ";".join(result) if all(isinstance(a, str) for a in result) else result
 
 
 def rearrange_name(name):
@@ -24,55 +49,81 @@ def rearrange_name(name):
     return name
 
 
+def get_attendee_names(attendees, role, partstat=None):
+    result = []
+    if isinstance(attendees, str):
+        if (role == "REQ-PARTICIPANT") and (partstat is None):
+            result.append(attendees.replace("mailto:", ""))
+    else:
+        if not isinstance(attendees, list):
+            attendees = [attendees]
+
+        for attendee in attendees:
+            # Default role to 'REQ-PARTICIPANT' if not specified
+            attendee_role = attendee.params.get("ROLE", "REQ-PARTICIPANT")
+            if attendee_role == role:
+                status = attendee.params.get("PARTSTAT", "NEEDS-ACTION")
+                if partstat is None or status == partstat:
+                    name = attendee.params.get("CN", "")
+                    rearranged_name = rearrange_name(name)
+                    result.append(rearranged_name)
+
+    return ";".join(result) if all(isinstance(a, str) for a in result) else result
+
+
 def parse_icalendar(file_path):
     with open(file_path, "rb") as file:
         calendar_data = Calendar.from_ical(file.read())
 
+    timezone_map = {}
     event_list = []
+
+    for component in calendar_data.walk():
+        if component.name == "VTIMEZONE":
+            timezone_id = component.get("TZID")
+            timezone_map[timezone_id] = {"standard": [], "daylight": []}
+            for sub_component in component.subcomponents:
+                if sub_component.name == "STANDARD":
+                    timezone_map[timezone_id]["standard"].append(
+                        {
+                            "DTSTART": format_value(sub_component.get("DTSTART").dt),
+                            "TZOFFSETFROM": format_value(
+                                sub_component.get("TZOFFSETFROM")
+                            ),
+                            "TZOFFSETTO": format_value(sub_component.get("TZOFFSETTO")),
+                            "RRULE": format_value(sub_component.get("RRULE")),
+                        }
+                    )
+                if sub_component.name == "DAYLIGHT":
+                    timezone_map[timezone_id]["daylight"].append(
+                        {
+                            "DTSTART": format_value(sub_component.get("DTSTART").dt),
+                            "TZOFFSETFROM": format_value(
+                                sub_component.get("TZOFFSETFROM")
+                            ),
+                            "TZOFFSETTO": format_value(sub_component.get("TZOFFSETTO")),
+                            "RRULE": format_value(sub_component.get("RRULE")),
+                        }
+                    )
 
     for component in calendar_data.walk():
         if component.name == "VEVENT":
             attendees = component.get("ATTENDEE", [])
-            mandatory_attendees = {
-                "ALL": set(),
-                "ACPT": set(),
-                "DECN": set(),
-                "TENT": set(),
-            }
-            optional_attendees = {
-                "ALL": set(),
-                "ACPT": set(),
-                "DECN": set(),
-                "TENT": set(),
-            }
 
-            for attendee in attendees:
-                if isinstance(attendee, str):
-                    email = attendee.replace("mailto:", "")
-                    role = "REQ-PARTICIPANT"
-                    partstat = "NEEDS-ACTION"
-                else:
-                    email = attendee.to_ical().decode().replace("mailto:", "")
-                    role = attendee.params.get("ROLE", "OPT-PARTICIPANT")
-                    partstat = attendee.params.get("PARTSTAT", "NEEDS-ACTION")
+            alarms = []
+            for sub_component in component.subcomponents:
+                if sub_component.name == "VALARM":
+                    alarms.append(
+                        {
+                            "Trigger": format_value(sub_component.get("TRIGGER")),
+                            "Action": format_value(sub_component.get("ACTION")),
+                            "Description": format_value(
+                                sub_component.get("DESCRIPTION")
+                            ),
+                        }
+                    )
 
-                if role == "REQ-PARTICIPANT":
-                    mandatory_attendees["ALL"].add(email)
-                    if partstat == "ACCEPTED":
-                        mandatory_attendees["ACPT"].add(email)
-                    elif partstat == "DECLINED":
-                        mandatory_attendees["DECN"].add(email)
-                    elif partstat == "TENTATIVE":
-                        mandatory_attendees["TENT"].add(email)
-                else:
-                    optional_attendees["ALL"].add(email)
-                    if partstat == "ACCEPTED":
-                        optional_attendees["ACPT"].add(email)
-                    elif partstat == "DECLINED":
-                        optional_attendees["DECN"].add(email)
-                    elif partstat == "TENTATIVE":
-                        optional_attendees["TENT"].add(email)
-
+            timezone_id = format_value(component.get("DTSTART").params.get("TZID"))
             event_details = {
                 "UID": format_value(component.get("UID")),
                 "Class": format_value(component.get("CLASS")),
@@ -82,7 +133,9 @@ def parse_icalendar(file_path):
                 "Location": format_value(component.get("LOCATION")),
                 "Organizer": {
                     "CN": (
-                        format_value(component.get("ORGANIZER").params.get("CN"))
+                        rearrange_name(
+                            format_value(component.get("ORGANIZER").params.get("CN"))
+                        )
                         if component.get("ORGANIZER")
                         else None
                     ),
@@ -99,7 +152,7 @@ def parse_icalendar(file_path):
                 },
                 "NameEvent": format_value(component.get("SUMMARY")),
                 "Description": format_value(component.get("DESCRIPTION")),
-                "Time_Zone_ID": format_value(component.get("TZID")),
+                "Time_Zone_ID": timezone_id,
                 "Begin_Time": format_value(component.get("DTSTART").dt),
                 "End_Time": format_value(component.get("DTEND").dt),
                 "Duration": format_value(component.get("DURATION")),
@@ -115,20 +168,85 @@ def parse_icalendar(file_path):
                 ),
                 "MandatoryAttendee": {
                     "mail": {
-                        "ALL": list(mandatory_attendees["ALL"]),
-                        "ACPT": list(mandatory_attendees["ACPT"]),
-                        "DECN": list(mandatory_attendees["DECN"]),
-                        "TENT": list(mandatory_attendees["TENT"]),
+                        "ALL": format_value(
+                            get_attendees(attendees, "REQ-PARTICIPANT")
+                        ),
+                        "NA": format_value(
+                            get_attendees(attendees, "REQ-PARTICIPANT", "NEEDS-ACTION")
+                        ),
+                        "ACPT": format_value(
+                            get_attendees(attendees, "REQ-PARTICIPANT", "ACCEPTED")
+                        ),
+                        "DECN": format_value(
+                            get_attendees(attendees, "REQ-PARTICIPANT", "DECLINED")
+                        ),
+                        "TENT": format_value(
+                            get_attendees(attendees, "REQ-PARTICIPANT", "TENTATIVE")
+                        ),
+                    },
+                    "CN": {
+                        "ALL": format_value(
+                            get_attendee_names(attendees, "REQ-PARTICIPANT")
+                        ),
+                        "NA": format_value(
+                            get_attendee_names(
+                                attendees, "REQ-PARTICIPANT", "NEEDS-ACTION"
+                            )
+                        ),
+                        "ACPT": format_value(
+                            get_attendee_names(attendees, "REQ-PARTICIPANT", "ACCEPTED")
+                        ),
+                        "DECN": format_value(
+                            get_attendee_names(attendees, "REQ-PARTICIPANT", "DECLINED")
+                        ),
+                        "TENT": format_value(
+                            get_attendee_names(
+                                attendees, "REQ-PARTICIPANT", "TENTATIVE"
+                            )
+                        ),
                     },
                 },
                 "OptionalAttendee": {
                     "mail": {
-                        "ALL": list(optional_attendees["ALL"]),
-                        "ACPT": list(optional_attendees["ACPT"]),
-                        "DECN": list(optional_attendees["DECN"]),
-                        "TENT": list(optional_attendees["TENT"]),
+                        "ALL": format_value(
+                            get_attendees(attendees, "OPT-PARTICIPANT")
+                        ),
+                        "NA": format_value(
+                            get_attendees(attendees, "OPT-PARTICIPANT", "NEEDS-ACTION")
+                        ),
+                        "ACPT": format_value(
+                            get_attendees(attendees, "OPT-PARTICIPANT", "ACCEPTED")
+                        ),
+                        "DECN": format_value(
+                            get_attendees(attendees, "OPT-PARTICIPANT", "DECLINED")
+                        ),
+                        "TENT": format_value(
+                            get_attendees(attendees, "OPT-PARTICIPANT", "TENTATIVE")
+                        ),
+                    },
+                    "CN": {
+                        "ALL": format_value(
+                            get_attendee_names(attendees, "OPT-PARTICIPANT")
+                        ),
+                        "NA": format_value(
+                            get_attendee_names(
+                                attendees, "OPT-PARTICIPANT", "NEEDS-ACTION"
+                            )
+                        ),
+                        "ACPT": format_value(
+                            get_attendee_names(attendees, "OPT-PARTICIPANT", "ACCEPTED")
+                        ),
+                        "DECN": format_value(
+                            get_attendee_names(attendees, "OPT-PARTICIPANT", "DECLINED")
+                        ),
+                        "TENT": format_value(
+                            get_attendee_names(
+                                attendees, "OPT-PARTICIPANT", "TENTATIVE"
+                            )
+                        ),
                     },
                 },
+                "Alarms": alarms,
             }
 
             event_list.append(event_details)
@@ -137,6 +255,6 @@ def parse_icalendar(file_path):
 
 
 # Example usage
-file_path = "/home/aashish/Documents/ics/parser/calendar3.ics"
+file_path = "/home/aashish/Documents/ics/parser/calendar2.ics"
 events = parse_icalendar(file_path)
 print(json.dumps(events, indent=4))
