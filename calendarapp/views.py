8fc6375e3974a9b5136e22883e496a3fe4aac936
Aashish Chapain <ashishchapain86@gmail.com>
2024-07-30 19:34:26 +0545

    ready for showcase

--- calendarapp/views.py
+++ calendarapp/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .parser import parse_icalendar  # Make sure this import is correct
+from .parser import parse_icalendar
 
 
 class ICSFileUploadView(APIView):
